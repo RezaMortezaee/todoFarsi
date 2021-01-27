5ca4974d80d88e8b81cfec0f679b2381d5d01b8f
RezaMrtz <reza.mrtz@gmail.com>
2021-01-27 22:49:11 +0330

    Add Jetsteam and Inertia

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
-use App\Models\Task;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -25,12 +31,14 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     * 
+     *
      * @var array
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -43,10 +51,11 @@ class User extends Authenticatable
     ];
 
     /**
-        Relations
-    */
-    public function tasks()
-    {
-        return $this->hasMany(Task::class);
-    }
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
